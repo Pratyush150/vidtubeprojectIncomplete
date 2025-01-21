7a50ba6ce0a3d21bb73d5c1143a1b4a0144b70c3
Pratyush Vatsa <pratyushvatsa121000@gmail.com>
2025-01-21 18:02:46 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
+// require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-import {app} from "./app.js";
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
-    path: "./.env"
+    path: './.env'
 })
 
-const PORT =  process.env.PORT  ||  8001
 
 
 connectDB()
 .then(() => {
-    app.listen(PORT, ()=>{
-            console.log(`server is running on pport ${PORT}`)
-         })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
 })
 .catch((err) => {
-    console.log("MongoDB connection error ", err)
+    console.log("MONGO db connection failed !!! ", err);
 })
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+*/
